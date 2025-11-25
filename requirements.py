520e593bcd540a04c652edbcdcf037a338dc3ef5
Ean van de Ven <ean2013@kpnmail.nl>
2025-11-25 12:38:24 +0100

    Added pip upgrading

--- requirements.py
+++ requirements.py
@@ -1,5 +1,6 @@
 # Installing required pip packages
 import os
+os.system("python3 -m pip install --upgrade pip")
 os.system("pip install pipreqs")
 os.system("pipreqs --force")
 os.system("pip install -r requirements.txt")
