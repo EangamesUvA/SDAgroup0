b8740de836538361f92d8ceca3b2b4ee5c53db29
Ean van de Ven <ean2013@kpnmail.nl>
2025-11-27 10:48:29 +0100

    Changed requirements to use python3 -m pip instead of pip

--- requirements.py
+++ requirements.py
@@ -1,7 +1,7 @@
 # Installing required pip packages
 import os
 os.system("python3 -m pip install -q --upgrade pip")
-os.system("pip install -q pipreqs")
+os.system("python3 -m pip install -q pipreqs")
 os.system("pipreqs --force")
-os.system("pip install -q -r requirements.txt")
+os.system("python3 -m pip install -q -r requirements.txt")
 del os
