6ed7fa5f748ec3ab83157edb54c89cb61593cede
Ean van de Ven <ean2013@kpnmail.nl>
2025-11-27 10:22:38 +0100

    Reading in dataset

--- data_visualize.py
+++ data_visualize.py
@@ -1,2 +1,17 @@
 from requirements import *
 
+import csv
+import numpy as np
+
+
+# Reading in a csv
+def read_csv(filename):
+    with open(filename, newline='') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+        lines = [row for row in spamreader]
+        labels = lines[0]
+        data = lines[1:]
+    return labels, data
+
+DATA_FILENAME = "data/ESS11e04_0-subset.csv"
+DATA_LABELS, DATASET = read_csv(DATA_FILENAME)
--- requirements.py
+++ requirements.py
@@ -1,7 +1,7 @@
 # Installing required pip packages
 import os
-os.system("python3 -m pip install --upgrade pip")
-os.system("pip install pipreqs")
+os.system("python3 -m pip install -q --upgrade pip")
+os.system("pip install -q pipreqs")
 os.system("pipreqs --force")
-os.system("pip install -r requirements.txt")
+os.system("pip install -q -r requirements.txt")
 del os
