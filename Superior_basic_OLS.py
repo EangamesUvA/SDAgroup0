e2f289eb0f8168e109e8dc5fd08035ff8531ed9c
Sashoela <alexandra.akha@gmail.com>
2025-12-17 20:32:29 +0100

    deleted plc

--- Superior_basic_OLS.py
+++ Superior_basic_OLS.py
@@ -20,7 +20,7 @@ df_clean = cleaner.full_clean()
 
 predictors = ['agea', 'hinctnta', 'edlvenl', 'edlvfenl', 'edlvmenl',
               'nwspol', 'netustm', 'vote', 'gndr']
-outcome = ['trstplc', 'trstplt']
+outcome = ['trstplt']
 
 continuous_vars = ['agea', 'nwspol', 'netustm', 'hinctnta', 'edlvenl', 'edlvfenl', 'edlvmenl']
 categorical_vars = ['vote', 'gndr'] 
@@ -56,11 +56,9 @@ X = df_clean[predictors_mod]
 X = sm.add_constant(X)
 
 y_trstplt = df_clean['trstplt']
-y_trstplc = df_clean['trstplc']
 
 # Fit models with HC3 robust standard errors
 model_trstplt = sm.OLS(y_trstplt, X).fit(cov_type='HC3')
-model_trstplc = sm.OLS(y_trstplc, X).fit(cov_type='HC3')
 
 #HC3 becuase we are dealing with social sciences
 #The Q-Q plot did not look promising, although we are dealing wiht a large dataset
@@ -68,7 +66,6 @@ model_trstplc = sm.OLS(y_trstplc, X).fit(cov_type='HC3')
 
 
 print(model_trstplt.summary())
-print(model_trstplc.summary())
 
 # ========================================
 # 6. Residual diagnostics
@@ -76,12 +73,9 @@ print(model_trstplc.summary())
 
 # Normality
 plot_qq(model_trstplt, dep_var_name='trstplt')
-plot_qq(model_trstplc, dep_var_name='trstplc')
 
 # Homoscedasticity
 plot_resid_vs_fitted(model_trstplt, dep_var_name='trstplt')
-plot_resid_vs_fitted(model_trstplc, dep_var_name='trstplc')
 
 # Outliers and Influence
 check_outliers_influence(model_trstplt, dep_var_name='trstplt')
-check_outliers_influence(model_trstplc, dep_var_name='trstplc')
